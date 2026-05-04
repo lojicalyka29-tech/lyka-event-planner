71d8279c5e53b55009dd304a494211e4543fa7d9
lojicalyka29-tech <lojicalyka29@gmail.com>
2026-05-04 11:40:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
         <section className="card">
           <header className="header">
             <img src={logo} alt="Lyka Lojica Events and Co." className="brand-logo" />
-            <h1>LYKA • JOHANNA • DOMA
+            <h1>LYKA
             Events & Co.</h1>
             <p className="subtle">Choose items, add them to cart, and submit in a clear 6-step flow.</p>
           </header>
